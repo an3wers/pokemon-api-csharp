95309c3dc1162784e45ac472d480f6411335e456
an3wers <an3wer@yandex.ru>
2023-11-06 19:10:36 +0500

    added models

---
+++ PokemonApi/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PokemonApi.Models
+{
+	public class Country
+	{
+		public int Id { get; set; }
+        public string Name { get; set; }
+		public ICollection<Owner> Owners { get; set; }
+
+    }
+}
+
